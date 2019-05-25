b264ee83d1cbaf278556db4ad0805c5f50be1330
Mateusz Bortkiewicz <allegromtbor@gmail.com>
2019-05-25 12:07:45 +0200

    Adding some changes to test merging

--- src/pl/sda/brudnopis1/Main.java
+++ src/pl/sda/brudnopis1/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Pobrano z Github, Projekt autorstwa mtbor1111");
+
         Metody metody = new Metody();
             metody.Start();
 
