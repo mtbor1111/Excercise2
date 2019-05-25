2e799f53cd614838ff9fd866efb044bd69df6d8d
mtbor1111 <49608266+mtbor1111@users.noreply.github.com>
2019-05-25 12:53:21 +0200

    Update Main.java

--- src/pl/sda/brudnopis1/Main.java
+++ src/pl/sda/brudnopis1/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Pobrano z Github, projekt autorstwa mtbor1111");
-        System.out.println("Pobrano z Github, projekt autorstwa mtbor1111");
 
         Metody metody = new Metody();
             metody.Start();
