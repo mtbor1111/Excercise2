6af1e0832e062d29212a20bb1f4d7a0531f92cbd
Mateusz Bortkiewicz <allegromtbor@gmail.com>
2019-06-02 13:40:47 +0200

    I'll try to fix this

---
+++ src/pl/sda/brudnopis1/Main.java
@@ -0,0 +1,13 @@
+package pl.sda.brudnopis1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pobrano z Github, projekt autorstwa mtbor1111");
+
+        Metody metody = new Metody();
+        metody.Start();
+
+    }
+}
